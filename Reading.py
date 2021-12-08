cf8b33ad773354223f9dc58617bef5984fde9392
marcpalomer <marcpalomer1999@gmail.com>
2021-12-08 12:37:30 +0100

    SVM working + Confusion matrix

--- Reading.py
+++ Reading.py
@@ -6,6 +6,7 @@ from scipy.interpolate import interp1d
 
 
 # Import datasets, classifiers and performance metrics
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 from os import chdir, getcwd
@@ -23,7 +24,7 @@ from os import chdir, getcwd
 
 path = '/Users/marcpalomercadenas/Desktop/TFG/TFG/archives/VARIABLES.xlsx'
 source = pd.read_excel(path)
-interpolation = ['ECG_HR', 'NIBP_MEAN','PROPO_CE']
+ 
 
 #Jo veig dues opcions de preparar les dades. Una primera que és la que estic fent
 #On a cada segon ens hauria de donar la probabilitat de que la resposta verbal fos negativa
@@ -58,10 +59,6 @@ def Finder(patient_dataframe, word1, word2):#, propo = True, remif = True):
 
     return idx
 
-def print_full(x):
-    pd.set_option('display.max_rows', 'display.max_cols')
-    print(x)
-    pd.reset_option('display.max_rows')
 
 def data_preprocessing(patient_dataframe,idx):
     #Busquem el index del primer valor de propo i si no hi és descartem el pacient
@@ -75,12 +72,12 @@ def data_preprocessing(patient_dataframe,idx):
         a = patient_dataframe.iloc[fvi:(idx+fvi),:]
         a.reset_index()
 
-        #Interpolation
-        for element in interpolation:
+        #Interpolation of variables
+        for element in ['ECG_HR', 'NIBP_MEAN','PROPO_CE']:
             a[element] = a[element].interpolate(axis = 0, method = 'linear')
             #a[element].fillnull(mean(a[element]), inplace = True)
         
-        #LoC binarisation
+        #LoC binarisation (creatinc a vector of 0 until negative verbal response, and the other are 1)
         vec1 = list(repeat(0,idx))
         vec2= repeat(1,idx)
         for element in vec2:
@@ -95,11 +92,15 @@ def data_preprocessing(patient_dataframe,idx):
     return a
 
 #Variable initialization
-patient_list = []
+general_dataframe = []
+X_train =[]
+X_test = []
+y_train = []
+y_test = []
 Verbal_and_propo = 0
 available_event = 0
 no_event = 0
-
+e = 0
 
 #Then we extract a list of filenames  from first csv to open all the others
 for patient_ID, event_state in zip(source['ID'],source['EVENTS']):
@@ -129,8 +130,43 @@ for patient_ID, event_state in zip(source['ID'],source['EVENTS']):
                 #We count the number of patients with verbal response in propo induction
 
                 Verbal_and_propo = Verbal_and_propo +1
-                #We append the 240 seconds patients' dataframes for later concatenation
-                patient_list.append(frame)
+                
+                
+                #We append the 240 seconds patients' dataframes for later concatenation and visualization
+                general_dataframe.append(frame)
+
+                #Input feature selection
+                selected_features = ['ECG_HR', 'NIBP_MEAN','PROPO_CE', 'LoC']
+                frame = frame[selected_features]
+
+                #CUIDADO QUE AQUI ESTEM PERDENT INFO
+                frame = frame.dropna()
+                
+                #As some patients use remifentanil, they fall as a whole set bc of NaN drop
+                if len(frame) >0:
+                    
+                    
+                    #Data scaling. There are a lot more types of scaling
+                    from sklearn.preprocessing import MaxAbsScaler
+                    scaler = MaxAbsScaler()
+                    frame = scaler.fit_transform(frame)
+
+                    # Split data into train and test subsets 
+                    X_train_patient, X_test_patient, y_train_patient, y_test_patient = train_test_split(frame , frame[:,-1], test_size=0.5, shuffle=False
+                    )
+                    X_train.append(X_train_patient)
+                    X_test.append(X_test_patient)
+                    y_train.append(y_train_patient)
+                    y_test.append(y_test_patient)
+
+                else:
+                    e = e+1
+                    
+                
+                
+
+                
+
 
     else:
         no_event = no_event +1
@@ -139,30 +175,12 @@ for patient_ID, event_state in zip(source['ID'],source['EVENTS']):
 print(f'There are {available_event}  patients from {available_event + no_event} with recorded events of any type')
 print(f'There are {round((Verbal_and_propo)*100/available_event,1)}% of patients from the {available_event} with recorded events presenting negative verbal response at some point while propo induction')
 
-#Concatenation the patients' data to introduce it to the ML algorithm
-input = pd.concat(patient_list)
-
-#Result feature declaration
-#input['LoC'] = 0
-
-#Input feature selection
-selected_features = ['ECG_HR', 'NIBP_MEAN','PROPO_CE', 'LoC']
-imput = input[selected_features]
-
-#CUIDADO QUE AQUI ESTEM PERDENT INFO
-imput = imput.dropna()
-
-#Data scaling. There are a lot more types of scaling
-from sklearn.preprocessing import MaxAbsScaler
-scaler = MaxAbsScaler()
-imput = scaler.fit_transform(imput)
-
-# Split data into train and test subsets
-
-##AIXÒ NO FUNCIONA GENS
-X_train, X_test, y_train, y_test = train_test_split(
-    imput, imput[:,-1], test_size=0.5, shuffle=False
-)
+#Concatenation the patients' data for viauslization
+input = pd.concat(general_dataframe)
+X_train = concatenate((X_train))
+X_test = concatenate(X_test)
+y_train = concatenate(y_train)
+y_test = concatenate(y_test)
 
 
 # Create a classifier: a support vector classifier
@@ -170,7 +188,6 @@ clf = svm.SVC(gamma=0.001)
 clf.fit(X_train, y_train)
 
 predicted = clf.predict(X_test)
-
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
@@ -180,12 +197,20 @@ print(
 # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
 # true digit values and the predicted digit values.
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+disp = ConfusionMatrixDisplay.from_predictions(y_test, predicted)
 disp.figure_.suptitle("Confusion Matrix")
 print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+plt.show()
 
 ##Comentaris
 # -1r agafar propo i remi ce, començar a agafar dades des d'on comença a infundir propo ce i si algun pacient no en té cap no agafarlo directament FET
 #Que les dades estiguin balancejades, igual num de dades abans i despres de l verbal (pel pairing i perque el model no fagi lu de la classificació gats gossos) FET
 #Interpolació de valors que no hi són FET
 #Tots els moments d'abans del verbal que siguin 0 i els altres 1. Comprovar fent debug que els frames que donen son correctes
+# I ara que fer?
+
+#Preguntes:
+#1- Com miro els valors dun dataframe al debug?
+#2- He de normalitzar respecte als maxims de cada pacient o respecte als maxims totals?
+#3- Com pot ser que la confusion matrix i els parametres del model siguin perfectes? Vaig pensar que era pel que m'havies comentat de fer el split per pacient i no, perque ara al canviarho passa el mateix.
