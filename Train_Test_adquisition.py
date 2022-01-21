d066cec0ace3accb44972e7a58c3029e629c6a85
marcpalomer <marcpalomer1999@gmail.com>
2022-01-21 10:02:44 +0100

    Fnal Update

--- Train_Test_adquisition.py
+++ Train_Test_adquisition.py
@@ -1,52 +1,36 @@
 from libraries import *
-#Hauria d'importar el primer CSV i veure a on hi ha events (pel training set)
-#Després fer un data frame juntant tots els csv?
-#I un model que m'indiques si el pacient està en fase de no resposta ciliar
-#SIntroduir una serie de variables a determinar (quines? Com es trien?)
-#Per tal que em dones una probabilitat de que la resposta palpedral fos negativa a  cada segon
-#És supervised perque es una classificació (?)
-#Com trio el model
 
-#Vull un classificador que agafant els features () em digui a cada segon si està dormit o no (classificacció binària)
-#No  ho farem amb ML temporal pk no dona millors resultats
-#Agafaré com dos minuts abans i dos després del LOC (resopsta verbal)
 
 path = '/Users/marcpalomercadenas/Desktop/TFG/TFG/archives/VARIABLES.xlsx'
 source = pd.read_excel(path)
- #Input feature selection
+
+#Input feature selection
 selected_features = ['ECG_HR', 'NIBP_MEAN','PROPO_CE', 'REMI_CE']
 
-#Jo veig dues opcions de preparar les dades. Una primera que és la que estic fent
-#On a cada segon ens hauria de donar la probabilitat de que la resposta verbal fos negativa
-#La segonaopció seria fer un dataframe on cada pacient fos una obsrvació i que tinguessim la
-#Ce (concentració efecte) a on s'ha detectat la perdua de resposta verbal i els segons que s'ha trigat 
-#des de l'inici de la infució. (Aixó estaria bé ficarho a analisis de possibilitats)
-def finder(patient_dataframe, word1, word2):#, propo = True, remif = True):
+#Function to find the index and the time where LoC occurs
+def finder(patient_dataframe, word1, word2):
+    
     patient_dataframe.reset_index(drop = True)
-    #We initialize the local variables and the error counter
-    #to know how many patients are being lost without verbal response
+
+    #We initialize the local variables
     idx = []
     a = 0
     b = 0
-
     
     #We search for "verb'" inside the event feature
     try:
         idx = list(patient_dataframe['EVENT']).index(word1)
-    
     except ValueError:
         a = 1
 
     #We search for "verbal" inside the event feature
     try :
         idx = list(patient_dataframe['EVENT']).index(word2)
-        
-
     except ValueError:
         b = 1
     
 
-    #If neither of the options has been useful to detecting the verbal response the error increases
+    #If neither of the options has been useful to detecting the verbal response nothing is returned
     if a*b == 1:
         idx = []
         LoC_time = 0
@@ -56,42 +40,43 @@ def finder(patient_dataframe, word1, word2):#, propo = True, remif = True):
 
     return [idx, LoC_time]
 
-# entering the df of a patient and moment o fverbal response (idx) and the desired features it returns the patient df  cassted to 120s and interposalted and the resulting output (LoC  = 0 or 1)
+#Data preprocessing before entering it to the model
 def data_preprocessing(patient_dataframe,idx,LoC_t,selected_features):
-    #Busquem el index del primer valor de propo i si no hi és descartem el pacient
     
+    #Busquem el index del primer valor de propo i si no hi és descartem el pacient
     fvi = patient_dataframe['PROPO_CE'].first_valid_index()
     
-    #Interpolation of variables
     
-
+    #Patient is only valid it it has propo induction and LoC happend after propo induction
     if fvi != None and idx > fvi:
 
-        #Aqui està agafpd.ant el index de 60 abans del verbal
-        
+        #Cast of the df
         a = patient_dataframe.loc[fvi:(2*idx-fvi),:]
         
         
-        #Interpolem Nans
+        #Interpolatio  Nans
         a.interpolate(method='linear', limit_direction='forward', axis=0, inplace = True)
 
-        #LoC binarisation (creatinc a vector of 0 until negative verbal response, and the other are 1)
-        #Generatinc LoC feature from 'Event' extraction
+        #LoC binarisation (creating a vector of 0 until negative verbal response, and the other are 1)
         index_LoC = list(a['TIME']).index(LoC_t)
         a.reset_index(drop = True, inplace = True)
-        
         LoC_index = list(a['TIME']).index(LoC_t)
         a['LoC']=0
         a.loc[LoC_index:,'LoC']=1
 
-        #CUIDADO QUE AQUI ESTEM PERDENT INFO, EN CONCRET TOT ELS QUE FAN PROPO I NO REMI
+        #Vdf of wanted variables
         a = a.loc[:,['ECG_HR', 'NIBP_MEAN','PROPO_CE', 'REMI_CE', 'LoC', 'TIME']]
+
+        #NaN handling
         a.loc[:,'REMI_CE'] = a.loc[:,'REMI_CE'].fillna(0)
         a = a.dropna()
         a.reset_index(drop = True, inplace = True)
+
+        #Variable assignation
         LoC = a['LoC']
         Time = a['TIME']
         a = a.loc[:,selected_features]
+
     else: 
         a = pd.DataFrame(columns= selected_features)
         LoC = pd.DataFrame()
@@ -112,6 +97,7 @@ no_event = 0
 e = 0
 available_index = 0
 available_patient = 0
+
 #Extracting patients df
 for patient_ID, event_state in zip(source['ID'],source['EVENTS']):
     if event_state == 'AVAILABLE':
--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from libraries import *
 from Train_Test_adquisition  import  data_preprocessing, finder
 
+#Loading models
 chdir("/Users/marcpalomercadenas/Desktop/TFG/TFG")
 with open('SVM_model.pickle', "rb") as file:
     SVM = pickle.load(file)
@@ -9,31 +10,47 @@ with open('RandomForest_model.pickle', "rb") as file:
     rfc = pickle.load(file)
 
 
-path = '/Users/marcpalomercadenas/Desktop/TFG/TFG/archives/CMA4_210310_083940.csv'
+path = '/Users/marcpalomercadenas/Desktop/TFG/TFG/archives/CMA4_201217_083154.csv'
 source = pd.read_csv(path)
 
 #Input feature selection
 selected_features = ['ECG_HR', 'NIBP_MEAN','PROPO_CE', 'REMI_CE']
 
+#Processing the data
 index, LoC_t  = finder(source, "verb'", "verbal")
 data_processed, LoC, Time = data_preprocessing(source, index, LoC_t,selected_features)
 
 data_processed.columns = ['0','1','2','3']
 
+#Normalisation
 from sklearn.preprocessing import MaxAbsScaler
 scaler = MaxAbsScaler()
 data = scaler.fit_transform(data_processed)
 
+#Model outcome
 predicted_values = rfc.predict_proba(data)
-plt.plot(Time, predicted_values[:,1])
-plt.plot(Time, LoC)
-
-plt.show()
-
 pred_LoC = predicted_values[:,1]
 
-#We set the threshold
+#Plotting of the probability
+plt.figure(dpi=1200) 
+plt.plot(Time, pred_LoC)
+plt.plot(Time, LoC)
+plt.legend(['SVM Prediction','Loss of verbal response'])
+plt.title('LoC Prediction Example')
+plt.xlabel('time since init Propofol infusion (s)')
+plt.ylabel('Probability of being unconscious')
+plt.savefig('LoC Example')
+plt.show()
+
+
+
+#Plotting with a threshold
+plt.figure(dpi=1200) 
 plt.plot(Time, pred_LoC > 0.85)
 plt.plot(Time, LoC)
-
+plt.legend(['SVM Prediction','Loss of verbal response'])
+plt.title('LoC Prediction Example')
+plt.xlabel('time since init Propofol infusion (s)')
+plt.ylabel('Probability of being unconscious. Threshold = 0.85')
+plt.savefig('LoC Example with Threshold')
 plt.show()
